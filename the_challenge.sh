8a44ddf32d5b1bc5ad40ca67989f54191eecb8f3
Shahid <shahidahmads1@gmail.com>
2016-10-06 17:09:05 -0400

    Update the_challenge.sh

--- the_challenge.sh
+++ the_challenge.sh
@@ -6,7 +6,7 @@
 #Purpose: This script displays products and their price listings. 
 #Execution time: the_challenge.sh takes approximately 4 minutes and 20 seconds to finish its execution on my machine i.e MacBook Pro 2.5GHz Intel Core i5 with Yosimite operating system.
 
-file="Products.txt"
+file="products.txt"
 model=""
 manufacturerArray=()
 manufacturersCount=0
