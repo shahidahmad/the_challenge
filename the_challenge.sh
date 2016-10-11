6e885bb0454fbacd74dead8dbadb922441956c29
Shahid <shahidahmads1@gmail.com>
2016-10-10 21:34:13 -0400

    Update the_challenge.sh

--- the_challenge.sh
+++ the_challenge.sh
@@ -4,7 +4,8 @@
 #Author: Shahid Khan
 #Date written: Sep 22 - Sep 26, 2016
 #Purpose: This script displays products and their price listings. 
-#Execution time: the_challenge.sh takes approximately 4 minutes and 20 seconds to finish its execution on my machine i.e MacBook Pro 2.5GHz Intel Core i5 with Yosimite operating system.
+#Updated: October 10, 2016
+#Execution time: This script takes around 5 minutes to complete its execution on my machine.
 
 file="products.txt"
 model=""
@@ -15,6 +16,7 @@ listingsCount=0
 
 echo -n "" | tee results.txt
 
+#This loop creates an array of manufacturers
 while read -r line;
 do
 	isPresent=false
@@ -28,7 +30,6 @@ do
      	else
           	for ((index=0; index < ${#manufacturerArray[@]}; index++))
 	  	do
-#             		echo "comparing " $(echo "$manufacturer" | tr '[:upper:]' '[:lower:]') " and " $(echo "${manufacturerArray[$index]}" | tr '[:upper:]' '[:lower:]')
               		if [[ $(echo "$manufacturer" | tr '[:upper:]' '[:lower:]') == $(echo "${manufacturerArray[$index]}" | tr '[:upper:]' '[:lower:]') ]]
               		then
                   		isPresent=true
@@ -45,11 +46,14 @@ do
      	echo "Manufacturers array: ${manufacturerArray[@]}"
 done<"$file"
 
+IFS='%' #I have changed IFS - Internal Field Seperator - default value to preserve white spaces in strings.
+
 for element in "${manufacturerArray[@]}"
 do
    	modelsArray=()
    	counter=0
 
+#Creates an array of models for an individual manufacturer
    	while read -r line;
    	do
      		model=$(echo $line |cut -d',' -f3 |cut -d':' -f2 |cut -d'"' -f2 |sed 's/[ \t]*$//')
@@ -59,42 +63,48 @@ do
       		((modelsCount++))
    	done< <(grep $element products.txt)  
     
-   	echo -e "Manufacture: $element\n, $element models: ${modelsArray[@]}"
-
    	text=$(cat listings.txt | grep -i "$element")
 	
    	for modelElement in "${modelsArray[@]}"
    	do
       		product_name=$(cat products.txt | sed -n "/\($element.*$modelElement\"\).*/{s//\1/p;q;}" | cut -d":" -f2 | cut -d'"' -f2)
-      		echo "product_name: $product_name"
-      		echo "product_model: $modelElement"
       		arrCount=0
 
       		if [[ ${#product_name} -ne 0 ]]
-      		then 
+      		then
        			while read -r modelText;
            		do
-				echo "model text: $modelText"
 				manufacturer=$(echo "$modelText" | cut -d'"' -f4 | cut -d' ' -f1 | tr -d ',:' | tr '[:upper:]' '[:lower:]')
 				manufacElement=$(echo "$element" | tr '[:upper:]' '[:lower:]')
-				echo "comparing $manufacturer and $manufacElement"
 
-				if [ "$manufacturer" = "$manufacElement" ];
+				if [ "$manufacturer" = "$manufacElement" ]
 				then
-	  				price=$( echo "$modelText" | rev | cut -d'"' -f2,6 |rev)
-	  				price=$(echo $price | awk -F '"' '{print $2$1 " "}')
-          				echo "$price added to $modelElement pricelistings array"
-	  				priceListingsArray[$arrCount]=$price
-	  				((arrCount++))
-	       				((listingsCount++))
+	  				price=$( echo "$modelText" | rev | cut -d'"' -f2 |rev)
+					currency=$(echo "$modelText" | rev | cut -d'"' -f6 | rev)
+					isCameraManufacturer=$(echo "$modelText" | awk 'BEGIN {FS="\":\""} {print $3}' | cut -d'"' -f1 | tr '[:upper:]' '[:lower:]')
+					title=$(echo "$modelText" | awk 'BEGIN {FS="\":\""} {print $2}' | awk 'BEGIN {FS="\",\"manufacturer"} {print $1}')
+					listing="{\"currency\":\"$currency\", \"price\":\"$price\", \"manufacturer\":\"$manufacturer\", \"title\":\"$title\"},"
+					echo "Model text:  $modelText"
+					echo "++++++++++++++ Comparing $manufacturer and $isCameraManufacturer +++++++++++++++++"	
+					if  [ "$manufacturer" = "$isCameraManufacturer" ]
+					then
+	  					priceListingsArray[$arrCount]=$listing
+	  					((arrCount++))
+	       					((listingsCount++))
+						echo "$title added to price listing array"
+					fi
 				fi	  
+
       	   		done< <(echo "$text" | grep -wi "$modelElement")
 
 	  		if [[ ${#priceListingsArray[@]} -ne 0 ]]
           		then
-	      			echo -e "{ \"product_name\": \"$product_name\", \"listing\":\"${priceListingsArray[@]}\"}" | tee -a results.txt
+				listingsLastElement=${priceListingsArray[${#priceListingsArray[@]}-1]}
+				listingsLastElement=$(echo $listingsLastElement | rev | cut -c2- | rev) #This statement removes comma found at the very end of the last listing in product's listings array.
+				priceListingsArray[${#priceListingsArray[@]}-1]=$listingsLastElement
+	      			echo -e "{ \"product_name\": \"$product_name\", \"listings\":[${priceListingsArray[@]}]}" | tee -a results.txt
 	  		else
-	      			echo -e "{ \"product_name\": \"$product_name\", \"listing\":null}" | tee -a results.txt
+	      			echo -e "{ \"product_name\": \"$product_name\", \"listings\":null}" | tee -a results.txt
           		fi
       		fi
 	
@@ -102,6 +112,9 @@ do
    	done
 done
 
+unset IFS
+
 echo "Total number of manufacturers: $manufacturersCount"
 echo "Total number of products: $modelsCount"
 echo "Total number of products price listings: $listingsCount" 
+
